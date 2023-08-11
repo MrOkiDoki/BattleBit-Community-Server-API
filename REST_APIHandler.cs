9be594c32bee296c44bbef15c2a833cf158763aa
caesarakalaeii <caesarlp@gmx.de>
2023-08-12 00:05:09 +0200

    comments

--- REST_APIHandler.cs
+++ REST_APIHandler.cs
@@ -49,7 +49,7 @@ class MyGameServer : GameServer<MyPlayer>
 
     public override async Task<bool> OnPlayerTypedMessage(MyPlayer player, ChatChannel channel, string msg)
     {
-        var c = new Command(); // just to test
+        var c = new Command(); // just to test replace with argument parsing for now
         c.Action.set("heal");
         c.Amount.set(10);
         c.ExecuterName.set("Tester");
