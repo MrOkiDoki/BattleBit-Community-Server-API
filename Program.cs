f73db4680fbfc26eeb7ed4f9925a643b41061958
caesarakalaeii <caesarlp@gmx.de>
2023-08-12 23:35:44 +0200

    fixing

--- Program.cs
+++ Program.cs
@@ -63,9 +63,9 @@ internal class MyGameServer : GameServer<MyPlayer>
     public override Task OnPlayerSpawned(MyPlayer player)
     {
         player.UpdateWeapon();
-        player.SetRunningSpeedMultiplier(1.5f);
+        player.SetRunningSpeedMultiplier(1.25f);
         player.SetFallDamageMultiplier(0f);
-        player.SetJumpMultiplier(2f);
+        player.SetJumpMultiplier(1.5f);
         return base.OnPlayerSpawned(player);
     }
 
