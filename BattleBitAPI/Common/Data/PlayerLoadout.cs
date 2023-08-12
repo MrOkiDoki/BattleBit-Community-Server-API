a25e11caa098eb451b798fbedc2c4657d34cc236
Rodion Kalyuzhin <tusur.rodion@gmail.com>
2023-08-13 03:03:17 +0700

    another copy paste fix

--- BattleBitAPI/Common/Data/PlayerLoadout.cs
+++ BattleBitAPI/Common/Data/PlayerLoadout.cs
@@ -282,7 +282,7 @@
                 case AttachmentType.Barrel:
                     return this.Barrel == attachment;
                 case AttachmentType.UnderRail:
-                    return this.Barrel == attachment;
+                    return this.UnderRail == attachment;
                 case AttachmentType.SideRail:
                     return this.SideRail == attachment;
                 case AttachmentType.Bolt:
