253b903f05984c6e425b1efdb0ec2d64957fc892
caesarakalaeii <caesarlp@gmx.de>
2023-08-14 00:18:31 +0200

    added faster way to start the api

---
+++ start_api.bat
@@ -0,0 +1,12 @@
+﻿git fetch
+
+git pull
+
+dotnet restore "CommunityServerAPI.csproj"
+
+dotnet build "CommunityServerAPI.csproj" --no-restore -c Release -o /app
+
+
+dotnet publish "CommunityServerAPI.csproj" -c Release -o /app
+
+dotnet run
