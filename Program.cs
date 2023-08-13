9e4229acc2000aace80f61e1e845dbb1f48befd3
DasIschBims <dasischbims@gmail.com>
2023-08-13 15:30:21 +0200

    added lifesteal base gamemode

--- Program.cs
+++ Program.cs
@@ -9,105 +9,199 @@ class Program
     static void Main(string[] args)
     {
         var listener = new ServerListener<MyPlayer, MyGameServer>();
-        listener.Start(29294);
+        listener.Start(30001);
+        
+        Console.WriteLine("API started!");
 
         Thread.Sleep(-1);
     }
 }
 class MyPlayer : Player<MyPlayer>
 {
-    public bool IsZombie;
+    public int Kills;
+    public int Deaths;
+    public List<MyPlayer> players;
 }
 class MyGameServer : GameServer<MyPlayer>
 {
-
-    public override async Task OnRoundStarted()
-    {
-    }
-    public override async Task OnRoundEnded()
-    {
-    }
-
-    public override async Task OnPlayerConnected(MyPlayer player)
-    {
-        bool anyZombiePlayer = false;
-        foreach (var item in AllPlayers)
-        {
-            if (item.IsZombie)
-            {
-                anyZombiePlayer = true;
-                break;
-            }
-        }
-
-        if (!anyZombiePlayer)
-        {
-            player.IsZombie = true;
-            player.Message("You are the zombie.");
-            player.Kill();
-        }
-    }
-
-    public override async Task OnAPlayerKilledAnotherPlayer(OnPlayerKillArguments<MyPlayer> args)
-    {
-        if (args.Victim.IsZombie)
-        {
-            args.Victim.IsZombie = false;
-            args.Victim.Message("You are no longer zombie");
-
-            AnnounceShort("Choosing new zombie in 5");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 4");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 3");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 2");
-            await Task.Delay(1000);
-            AnnounceShort("Choosing new zombie in 1");
-            await Task.Delay(1000);
-
-            args.Killer.IsZombie = true;
-            args.Killer.SetHeavyGadget(Gadgets.SledgeHammer.ToString(), 0, true);
-
-            var position = args.Killer.GetPosition();
-        }
-    }
-
-
-    public override async Task<OnPlayerSpawnArguments> OnPlayerSpawning(MyPlayer player, OnPlayerSpawnArguments request)
-    {
-        if (player.IsZombie)
-        {
-            request.Loadout.PrimaryWeapon = default;
-            request.Loadout.SecondaryWeapon = default;
-            request.Loadout.LightGadget = null;
-            request.Loadout.HeavyGadget = Gadgets.SledgeHammer;
-            request.Loadout.Throwable = null;
-        }
-
-        return request;
-    }
-    public override async Task OnPlayerSpawned(MyPlayer player)
-    {
-        if(player.IsZombie)
-        {
-            player.SetRunningSpeedMultiplier(2f);
-            player.SetJumpMultiplier(2f);
-            player.SetFallDamageMultiplier(0f);
-            player.SetReceiveDamageMultiplier(0.1f);
-            player.SetGiveDamageMultiplier(4f);
-        }
-    }
-
-
-
+    private List<MyPlayer> players;
+    
     public override async Task OnConnected()
     {
-        await Console.Out.WriteLineAsync("Current state: " + RoundSettings.State);
+        Console.WriteLine($"Gameserver connected! {this.GameIP}:{this.GamePort}");
 
+        ServerSettings.BleedingEnabled = false;
+        ServerSettings.SpectatorEnabled = false;
     }
+    
+    public override async Task OnDisconnected()
+    {
+        Console.WriteLine($"Gameserver disconnected! {this.GameIP}:{this.GamePort}");
+    }
+
+    public override async Task OnReconnected()
+    {
+        Console.WriteLine($"Gameserver reconnected! {this.GameIP}:{this.GamePort}");
+        
+        ServerSettings.BleedingEnabled = false;
+        ServerSettings.SpectatorEnabled = false;
+    }
+
     public override async Task OnGameStateChanged(GameState oldState, GameState newState)
     {
         await Console.Out.WriteLineAsync("State changed to -> " + newState);
+    }
+
+    public override async Task OnTick()
+    {
+        var top5 = players.OrderByDescending(x => x.Kills / x.Deaths).Take(5).ToList();
+    
+        string announcement = $"<align=\"center\">--- Top 5 Players ---\n" +
+                              $"1. {top5[0].Name} - {top5[0].Kills / top5[0].Deaths}\n" +
+                              $"2. {top5[1].Name} - {top5[1].Kills / top5[1].Deaths}\n" +
+                              $"3. {top5[2].Name} - {top5[2].Kills / top5[2].Deaths}\n" +
+                              $"4. {top5[3].Name} - {top5[3].Kills / top5[3].Deaths}\n" +
+                              $"5. {top5[4].Name} - {top5[4].Kills / top5[4].Deaths}\n</align>";
+
+        AnnounceShort(announcement);
+    }
+
+    public override async Task <bool> OnPlayerTypedMessage(MyPlayer player, ChatChannel channel, string msg)
+    {
+        if (player.SteamID != 76561198395073327 || !msg.StartsWith("/")) return true;
+        
+        var words = msg.Split(" ");
+        
+        var type = ulong.TryParse(words[1], out var steamid) ? "steamid" : "name";
+
+        switch (words[0])
+        {
+            case "/tp":
+                // /tp <steamid/name>
+                var tpTarget = type == "steamid" ? players.FirstOrDefault(x => x.SteamID == steamid) : players.FirstOrDefault(x => x.Name == words[1]);
+                
+                if (tpTarget == null)
+                {
+                    player.Message("Player not found!");
+                    return false;
+                }
+                
+                player.Message("Not implemented yet!");
+                break;
+            
+            case "/kill":
+                // /kill <steamid/name>
+                var killTarget = type == "steamid" ? players.FirstOrDefault(x => x.SteamID == steamid) : players.FirstOrDefault(x => x.Name == words[1]);
+                
+                if (killTarget == null)
+                {
+                    player.Message("Player not found!");
+                    return false;
+                }
+                
+                killTarget.Kill();
+                break;
+            
+            case "/heal":
+                // /heal <steamid/name> <amount>
+                var healTarget = type == "steamid" ? players.FirstOrDefault(x => x.SteamID == steamid) : players.FirstOrDefault(x => x.Name == words[1]);
+                
+                if (healTarget == null)
+                {
+                    player.Message("Player not found!");
+                    return false;
+                }
+                
+                var healAmount = words.Length > 2 ? int.Parse(words[2]) : 100;
+                healTarget.Heal(healAmount);
+                break;
+            
+            case "/kick":
+                // /kick <steamid/name> <reason>
+                var kickTarget = type == "steamid" ? players.FirstOrDefault(x => x.SteamID == steamid) : players.FirstOrDefault(x => x.Name == words[1]);
+                
+                if (kickTarget == null)
+                {
+                    player.Message("Player not found!");
+                    return false;
+                }
+                
+                var kickReason = words.Length > 2 ? string.Join(" ", words.Skip(2)) : "No reason provided";
+                
+                kickTarget.Kick(kickReason);
+                break;
+            default:
+                player.Message("Unknown command!");
+                break;
+        }
+        
+        return false;
+    }
+
+    public override async Task OnRoundStarted()
+    {
+        players = AllPlayers.ToList();
+
+        foreach (var player in players)
+        {
+            player.Kills = 0;
+            player.Deaths = 0;
+        }
+    }
+
+
+    public override async Task OnPlayerConnected(MyPlayer player)
+    {
+        player.Kills = 0;
+        player.Deaths = 0;
+    }
+
+    public override Task<PlayerStats> OnGetPlayerStats(ulong steamID, PlayerStats officialStats)
+    {
+        var stats = new PlayerStats();
+
+        stats.Progress.Rank = 200;
+        stats.Progress.Prestige = 10;
+        stats.Progress.EXP = uint.MaxValue;
+        if (steamID == 76561198395073327)
+        {
+            stats.Roles = Roles.Admin;
+        }
+        
+        return Task.FromResult(stats);
+    }
+    public override async Task OnAPlayerKilledAnotherPlayer(OnPlayerKillArguments<MyPlayer> args)
+    {
+        args.Killer.Heal(100);
+        args.Killer.Kills++;
+        args.Victim.Deaths++;
+    }
+
+    public override async Task <bool> OnPlayerRequestingToChangeRole(MyPlayer player, GameRole role)
+    {
+        if (role == GameRole.Assault)
+            return true;
+        
+        player.GameServer.AnnounceShort("You can only play as Assault!");
+        return false;
+    }
+
+    public override async Task<OnPlayerSpawnArguments> OnPlayerSpawning(MyPlayer player, OnPlayerSpawnArguments request)
+    {
+        request.Loadout.PrimaryWeapon = default;
+        request.Loadout.SecondaryWeapon = default;
+        request.Loadout.LightGadget = null;
+        request.Loadout.HeavyGadget = Gadgets.AdvancedBinoculars;
+        request.Loadout.Throwable = null;
+        request.Loadout.FirstAid = null;
+        request.Loadout.PrimaryExtraMagazines = 5;
+        request.Loadout.SecondaryExtraMagazines = 5;
+
+        return request;
+    }
+    
+    public override async Task OnPlayerSpawned(MyPlayer player)
+    {
+        player.SetGiveDamageMultiplier(0.25f);
     }
 }
