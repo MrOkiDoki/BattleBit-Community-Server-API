9c0d0eeb7e85b1b6b937e3129ea2cd9183691412
DasIschBims <dasischbims@gmail.com>
2023-08-17 21:47:40 +0200

    add text

--- Commands.cs
+++ Commands.cs
@@ -66,7 +66,7 @@ public class KillCommand : ApiCommand
     public KillCommand()
     {
         CommandString = "/kill";
-        HelpString = "/kill <name>|<steamid>";
+        HelpString = "Kill player by name or steamid";
         Aliases = new string[] { "/k" };
         AdminOnly = true;
     }
